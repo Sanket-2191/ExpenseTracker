fd4454c55446cb0d1fd0fd420c7849e785676fa7
sanket-2191 <sanketpadole1@gmail.com>
2025-06-02 09:13:02 +0530

    analytics dashboard bug fix.

--- src/api/analyticsAPI.js
+++ src/api/analyticsAPI.js
@@ -4,25 +4,25 @@ const domain = import.meta.env.VITE_API_URL;
 const API_URL = `${domain}/analytics-dashboard`;
 
 export const getBudgetOverview = (month, year) =>
-    axios.get(`${domain}/overview`, {
+    axios.get(`${API_URL}/overview`, {
         withCredentials: true,
         params: { month, year },
     });
 
 export const getOverspentCategories = (month, year) =>
-    axios.get(`${domain}/overspent`, {
+    axios.get(`${API_URL}/overspent`, {
         withCredentials: true,
         params: { month, year },
     });
 
 export const getBudgetVsTransaction = (month, year) =>
-    axios.get(`${domain}/compare`, {
+    axios.get(`${API_URL}/compare`, {
         withCredentials: true,
         params: { month, year },
     });
 
 export const getMonthlyTrends = (year) =>
-    axios.get(`${domain}/monthly-trends`, {
+    axios.get(`${API_URL}/monthly-trends`, {
         withCredentials: true,
         params: { year },
     });
